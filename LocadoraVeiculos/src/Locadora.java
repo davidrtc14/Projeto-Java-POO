35e6d61f1b13ff0b3d15073fc98b0e30374760e6
davidrtc14 <davidrtc14@gmail.com>
2024-05-22 22:22:00 -0300

    Adicao dos Metodos de Cadastro e Gerenciamento de Funcionarios

--- LocadoraVeiculos/src/Locadora.java
+++ LocadoraVeiculos/src/Locadora.java
@@ -5,9 +5,12 @@ import java.util.List;
 public class Locadora {
 
     private List<Cliente> listaClientes;
+    private List<Funcionario> listaFuncionarios;
 
     public Locadora() {
         this.listaClientes = new ArrayList<>();
+        this.listaFuncionarios = new ArrayList<>();
+        settarFuncionarios();
 
     }
 
@@ -32,6 +35,7 @@ public class Locadora {
                 exibirMenuClientes();
                 break;
             case 2:
+                autenticarFuncionario(scanner);
                 break;
             case 3:
                 System.out.println("Saindo...");
@@ -123,5 +127,93 @@ public class Locadora {
                 System.out.println(cliente.exibirInfoCliente());
             }
         }
+    }
+
+    private void exibirMenuFuncionarios() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Selecione uma das opções abaixo:");
+        System.out.println("1. Cadastrar Funcionário");
+        System.out.println("2. Cadastrar Veículo");
+        System.out.println("3. Exibir Locações");
+        System.out.println("4. Exibir Clientes");
+        System.out.println("5. Voltar");
+
+        int opcao = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (opcao) {
+            case 1:
+                cadastrarFuncionario(scanner);
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                exibirClientes();
+                break;
+            case 5:
+                exibirMenuInicial();
+                break;
+            default:
+                System.out.println("Opção inválida.");
+                exibirMenuFuncionarios();
+        }
+    }
+
+    private void settarFuncionarios() {
+        Funcionario f1 = new Funcionario("David Ramalho Teixeira de Carvalho", "drtc@locveiculos.com", "123");
+        Funcionario f2 = new Funcionario("Arthur Aurélio de Lima", "aadl@locveiculos.com", "456");
+        Funcionario f3 = new Funcionario("Arthur Nobrega Martins", "anm@locveiculos.com", "789");
+        Funcionario f4 = new Funcionario("Hygor Gabriel Alves Montenegro", "hgam@locveiculos.com", "321");
+        Funcionario f5 = new Funcionario("Leonardo Felipe Silva Costa", "lfsc@locveiculos.com", "654");
+        Funcionario f6 = new Funcionario("Marcos Pereira Matias Neto", "mpmn@locveiculos.com", "senha123");
+
+        listaFuncionarios.add(f1);
+        listaFuncionarios.add(f2);
+        listaFuncionarios.add(f3);
+        listaFuncionarios.add(f4);
+        listaFuncionarios.add(f5);
+        listaFuncionarios.add(f6);
+    }
+
+    private void autenticarFuncionario(Scanner scanner) {
+        System.out.println("Digite o seu Email:");
+        String email = scanner.nextLine();
+        System.out.println("Digite a sua Senha:");
+        String senha = scanner.nextLine();
+
+        Funcionario funcionario = encontrarFuncionario(email, senha);
+        if (funcionario != null) {
+            exibirMenuFuncionarios();
+        } else {
+            System.out.println("Email ou senha incorretos. Tente novamente.");
+            exibirMenuInicial();
+        }
+    }
+
+    private Funcionario encontrarFuncionario(String email, String senha) {
+        for (Funcionario funcionario : listaFuncionarios) {
+            if (funcionario.getEmail().equals(email) && funcionario.getSenha().equals(senha)) {
+                return funcionario;
+            }
+        }
+        return null;
+    }
+
+    private void cadastrarFuncionario(Scanner scanner) {
+        System.out.println("Digite o nome completo do funcionário:");
+        String nomeCompleto = scanner.nextLine();
+        System.out.println("Digite o e-mail do funcionário:");
+        String email = scanner.nextLine();
+        System.out.println("Digite a senha do funcionário:");
+        String senha = scanner.nextLine();
+
+        Funcionario funcionario = new Funcionario(nomeCompleto, email, senha);
+
+        listaFuncionarios.add(funcionario);
+        System.out.println("Funcionário cadastrado com sucesso!");
+        exibirMenuFuncionarios();
     }
 }
