d559206b6d1ab4e0fd50cff08b957e48c39f551e
ARTHURNOBREGA1268 <arthurnobrega63@gmail.com>
2024-05-22 21:57:21 -0300

    Criacao do Menu Principal

--- LocadoraVeiculos/src/Locadora.java
+++ LocadoraVeiculos/src/Locadora.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class Locadora {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Locadora locadora = new Locadora();
+        locadora.exibirMenuInicial();
+    }
+
+    private void exibirMenuInicial() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Olá, Bem-Vindo a Nossa Locadora de Veículos, selecione uma das opções abaixo:");
+        System.out.println("1. Sou Cliente");
+        System.out.println("2. Sou Funcionário");
+        System.out.println("3. Sair");
+
+        int opcao = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (opcao) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                System.out.println("Saindo...");
+                break;
+            default:
+                System.out.println("Opção inválida.");
+                exibirMenuInicial();
+        }
     }
 }
