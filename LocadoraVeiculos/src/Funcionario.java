4c64c0028b0e54da6ce6144a1edb6043d99f218f
hg0407 <ahygor31@gmail.com>
2024-05-22 21:28:32 -0300

    Abstracoes, getters e setters

--- LocadoraVeiculos/src/Funcionario.java
+++ LocadoraVeiculos/src/Funcionario.java
@@ -1,2 +1,33 @@
 public class Funcionario {
+    private String nomeCompleto, email, senha;
+
+    public Funcionario(String nomeCompleto, String email, String senha) {
+        this.nomeCompleto = nomeCompleto;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
